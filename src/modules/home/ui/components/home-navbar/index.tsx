5c3475c6815f675695f0bb549882bb4246f6eca9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-11 15:58:42 +0300

    Made some changes for the react component

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -1,3 +1,29 @@
+import { SidebarTrigger } from "@/components/ui/sidebar";
+import { SearchInput } from "@/modules/home/ui/components/home-navbar/search-input";
+import Image from "next/image";
+import Link from "next/link";
+
 export const HomeNavbar = () => {
-	return <div>Home Navbar</div>;
+	return (
+		<nav className="fixed top-0 left-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50">
+			<div className="flex items-center gap-4 w-full">
+				{/* Menu and logo */}
+				<div className="flex items-center flex-shrink-0">
+					<SidebarTrigger />
+					<Link href="/">
+						<div className="p-4 flex items-center gap-1">
+							<Image src="/logo.svg" alt="logo" width={32} height={32} />
+							<p className="text-xl font-semibold tracking-tight">NewTube</p>
+						</div>
+					</Link>
+				</div>
+
+				{/* Search Bar */}
+				<div className="flex-1 flex justify-center max-w-[700px] mx-auto">
+					<SearchInput />
+				</div>
+			</div>
+			<div className="flex-shrink-0 items-center flex gap-4"></div>
+		</nav>
+	);
 };
