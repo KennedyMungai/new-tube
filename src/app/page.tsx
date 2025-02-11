8aa581e0088b2ccae6ad5e2e4ee784e4c97efa9d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-11 15:07:59 +0300

    Added the app logo to the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+
 const HomePage = () => {
-	return <div>HomePage</div>;
+	return (
+		<div>
+			<Image src="/logo.svg" height={50} width={50} alt="Logo" />
+			<p className="text-xl font-semibold tracking-tight">NewTube</p>
+		</div>
+	);
 };
 
 export default HomePage;
