46ce23263c8951642010a90a0869a8e4b8ec554e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-11 15:59:22 +0300

    Rendering HomeLayout inside the HomeRouteLayout

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,11 +1,12 @@
+import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
 import { ReactNode } from "react";
 
 type Props = {
 	children: ReactNode;
 };
 
-const HomeLayout = ({ children }: Props) => {
-	return <div>{children}</div>;
+const HomeRouteLayout = ({ children }: Props) => {
+	return <HomeLayout>{children}</HomeLayout>;
 };
 
-export default HomeLayout;
+export default HomeRouteLayout;
