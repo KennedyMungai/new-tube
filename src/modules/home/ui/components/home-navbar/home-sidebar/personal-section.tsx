afeccf8024bbcc5bd511c9af96d3b8ebeb94e784
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-12 09:07:00 +0300

    Added and rendered the personal section of the sidebar

---
+++ src/modules/home/ui/components/home-navbar/home-sidebar/personal-section.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+	HistoryIcon,
+	ListVideoIcon,
+	ThumbsUpIcon,
+	type LucideIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+const items: {
+	title: string;
+	url: string;
+	icon: LucideIcon;
+	auth?: boolean;
+}[] = [
+	{
+		title: "History",
+		url: "/playlists/history",
+		icon: HistoryIcon,
+		auth: true,
+	},
+	{
+		title: "Liked Videos",
+		url: "/playlists/liked",
+		icon: ThumbsUpIcon,
+		auth: true,
+	},
+	{
+		title: "All PLaylists",
+		url: "/playlists",
+		icon: ListVideoIcon,
+		auth: true,
+	},
+];
+
+export const PersonalSection = () => {
+	return (
+		<SidebarGroup>
+			<SidebarGroupLabel>You</SidebarGroupLabel>
+			<SidebarGroupContent>
+				<SidebarMenu>
+					{items.map((item) => (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton
+								tooltip={item.title}
+								isActive={false} // TODO: Change to look at current pathname
+								asChild
+								onClick={() => {}} // TODO: Add click handler
+							>
+								<Link href={item.url} className="flex items-center gap-4">
+									<item.icon />
+									<span className="text-sm">{item.title}</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					))}
+				</SidebarMenu>
+			</SidebarGroupContent>
+		</SidebarGroup>
+	);
+};
