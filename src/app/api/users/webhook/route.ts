89818ef958009536da3e586e5ab3da26635a1d54
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-12 14:18:39 +0300

    Webhook db operations

--- src/app/api/users/webhook/route.ts
+++ src/app/api/users/webhook/route.ts
@@ -1,4 +1,7 @@
+import { db } from "@/db";
+import { users } from "@/db/schema";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
 import { Webhook } from "svix";
 
@@ -49,10 +52,38 @@ export async function POST(req: Request) {
 
 	// Do something with payload
 	// For this guide, log payload to console
-	const { id } = evt.data;
 	const eventType = evt.type;
-	console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-	console.log("Webhook payload:", body);
+
+	if (eventType === "user.created") {
+		const { data } = evt;
+		await db.insert(users).values({
+			clerkId: data.id,
+			name: `${data.first_name} ${data.last_name}`,
+			imageUrl: data.image_url,
+		});
+	}
+
+	if (eventType === "user.deleted") {
+		const { data } = evt;
+
+		if (!data.id) {
+			return new Response("Missing user id", { status: 404 });
+		}
+
+		await db.delete(users).where(eq(users.clerkId, data.id));
+	}
+
+	if (eventType === "user.updated") {
+		const { data } = evt;
+
+		await db
+			.update(users)
+			.set({
+				name: `${data.first_name} ${data.last_name}`,
+				imageUrl: data.image_url,
+			})
+			.where(eq(users.clerkId, data.id));
+	}
 
 	return new Response("Webhook received", { status: 200 });
 }
