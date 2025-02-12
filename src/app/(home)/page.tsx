90d3ed84a1af4122eb88ae1ba1d8a4fa684ebd5a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-12 09:08:18 +0300

    Updated the index page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,5 @@
-import Image from "next/image";
-
 const HomePage = () => {
-	return (
-		<div>
-			<Image src="/logo.svg" height={50} width={50} alt="Logo" />
-			<p className="text-xl font-semibold tracking-tight">NewTube</p>
-		</div>
-	);
+	return <div>I will load videos in the future</div>;
 };
 
 export default HomePage;
