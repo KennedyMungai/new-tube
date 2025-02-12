f66e924b301b6305cf0aa2d4a41f799c9ccaee1e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-12 08:58:30 +0300

    Created and rendered the main-section component

---
+++ src/modules/home/ui/components/home-navbar/home-sidebar/main-section.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import {
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import {
+	FlameIcon,
+	HomeIcon,
+	PlaySquareIcon,
+	type LucideIcon,
+} from "lucide-react";
+import Link from "next/link";
+
+const items: {
+	title: string;
+	url: string;
+	icon: LucideIcon;
+	auth?: boolean;
+}[] = [
+	{
+		title: "Home",
+		url: "/",
+		icon: HomeIcon,
+	},
+	{
+		title: "Subscriptions",
+		url: "/feed/subscriptions",
+		icon: PlaySquareIcon,
+		auth: true,
+	},
+	{
+		title: "Trending",
+		url: "/feed/trending",
+		icon: FlameIcon,
+	},
+];
+
+export const MainSection = () => {
+	return (
+		<SidebarGroup>
+			<SidebarGroupContent>
+				<SidebarMenu>
+					{items.map((item) => (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton
+								tooltip={item.title}
+								isActive={false}
+								asChild
+								onClick={() => {}}>
+								<Link href={item.url} className="flex items-center gap-4">
+									<item.icon />
+									<span className="text-sm">{item.title}</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					))}
+				</SidebarMenu>
+			</SidebarGroupContent>
+		</SidebarGroup>
+	);
+};
