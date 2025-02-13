6a1f1f90fd1f5b8d44343137b8c31ceb09ad35d2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 15:06:38 +0300

    Removed the border-none sty;e

--- src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
@@ -5,7 +5,7 @@ import { PersonalSection } from "@/modules/home/ui/components/home-navbar/home-s
 
 export const StudioSidebar = () => {
 	return (
-		<Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+		<Sidebar className="pt-16 z-40" collapsible="icon">
 			<SidebarContent className="bg-background">
 				<MainSection />
 				<Separator />
