d227eb406d476381445387700b42ae596759c526
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 10:06:21 +0300

    The categories seeding script

--- src/scripts/seed-categories.ts
+++ src/scripts/seed-categories.ts
@@ -1,1 +1,40 @@
+import { db } from "@/db";
+import { categories } from "@/db/schema";
+
 // TODO: Create a script to seed categories
+const categoryNames = [
+	"Cars and Vehicles",
+	"Comedy",
+	"Education",
+	"Gaming",
+	"Entertainment",
+	"Film and Animation",
+	"How-to and Style",
+	"Music",
+	"News and Politics",
+	"People and Blogs",
+	"Pets and Animals",
+	"Science and Technology",
+	"Sports",
+	"Travel and Events",
+];
+
+async function main() {
+	console.log("Seeding categories...");
+
+	try {
+		const values = categoryNames.map((name) => ({
+			name,
+			description: `Videos on ${name}`,
+		}));
+
+		await db.insert(categories).values(values);
+
+		console.log("Categories seeded successfully");
+	} catch (error) {
+		console.error("Error seeding categories: ", error);
+		process.exit(1);
+	}
+}
+
+main();
