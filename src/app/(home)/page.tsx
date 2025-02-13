ca24ed90c99a5abbb9a8b150cc5f20d969c19710
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 10:19:04 +0300

    Removed the PageClient component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,20 +1,9 @@
-import { PageClient } from "@/app/(home)/client";
 import { HydrateClient, trpc } from "@/trpc/server";
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 
 const HomePage = async () => {
 	void trpc.categories.getMany.prefetch();
 
-	return (
-		<HydrateClient>
-			<Suspense fallback={<p>Loading...</p>}>
-				<ErrorBoundary fallback={<div>Something went wrong</div>}>
-					<PageClient />
-				</ErrorBoundary>
-			</Suspense>
-		</HydrateClient>
-	);
+	return <HydrateClient></HydrateClient>;
 };
 
 export default HomePage;
