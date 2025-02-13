ef2b56a028362845f944877d9742c35bb69f8c15
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 14:26:42 +0300

    Created and rendered the filer-carousel

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -1,13 +1,31 @@
 "use client";
 
+import { FilterCarousel } from "@/components/filter-carousel";
 import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 type Props = {
 	categoryId?: string;
 };
 
 export const CategoriesSection = ({ categoryId }: Props) => {
+	return (
+		<Suspense fallback={<p>Loading...</p>}>
+			<ErrorBoundary fallback={<p>Error...</p>}>
+				<CategoriesSectionSuspense categoryId={categoryId} />
+			</ErrorBoundary>
+		</Suspense>
+	);
+};
+
+const CategoriesSectionSuspense = ({ categoryId }: Props) => {
 	const [categories] = trpc.categories.getMany.useSuspenseQuery();
 
-	return <div>{JSON.stringify(categories)}</div>;
+	const data = categories.map(({ name, id }) => ({
+		value: id,
+		label: name,
+	}));
+
+	return <FilterCarousel value={categoryId} data={data} />;
 };
