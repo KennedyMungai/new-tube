20467f66225a4ac7d47caf95d26f7caf88b95f7e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 09:21:55 +0300

    Made some changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-	return <div>I will load videos in the future</div>;
+import { PageClient } from "@/app/(home)/client";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
+const HomePage = async () => {
+	void trpc.hello.prefetch({ text: "chickens" });
+
+	return (
+		<HydrateClient>
+			<Suspense fallback={<p>Loading...</p>}>
+				<ErrorBoundary fallback={<div>Something went wrong</div>}>
+					<PageClient />
+				</ErrorBoundary>
+			</Suspense>
+		</HydrateClient>
+	);
 };
 
 export default HomePage;
