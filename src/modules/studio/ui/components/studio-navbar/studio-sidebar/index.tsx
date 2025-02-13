3ee80301b450e6e3b71f99075026804bddfe9e4f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 15:07:54 +0300

    Even more refactoring

--- src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
@@ -1,7 +1,7 @@
 import { Separator } from "@/components/ui/separator";
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import { MainSection } from "@/modules/home/ui/components/home-navbar/home-sidebar/main-section";
-import { PersonalSection } from "@/modules/home/ui/components/home-navbar/home-sidebar/personal-section";
+import { MainSection } from "@/modules/studio/ui/components/studio-navbar/studio-sidebar/main-section";
+import { PersonalSection } from "@/modules/studio/ui/components/studio-navbar/studio-sidebar/personal-section";
 
 export const StudioSidebar = () => {
 	return (
