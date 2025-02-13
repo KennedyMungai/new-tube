faf16433ea217f4406ded3861cce5e4c208802f6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-13 15:18:58 +0300

    Made some changes to the project

--- src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-navbar/studio-sidebar/index.tsx
@@ -1,15 +1,46 @@
-import { Separator } from "@/components/ui/separator";
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
-import { MainSection } from "@/modules/studio/ui/components/studio-navbar/studio-sidebar/main-section";
-import { PersonalSection } from "@/modules/studio/ui/components/studio-navbar/studio-sidebar/personal-section";
+"use client";
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { LogOutIcon, VideoIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export const StudioSidebar = () => {
+	const pathname = usePathname();
+
 	return (
 		<Sidebar className="pt-16 z-40" collapsible="icon">
 			<SidebarContent className="bg-background">
-				<MainSection />
-				<Separator />
-				<PersonalSection />
+				<SidebarGroup>
+					<SidebarMenu>
+						<SidebarMenuItem>
+							<SidebarMenuButton
+								tooltip={"Exit Studio"}
+								isActive={pathname === "/studio"}
+								asChild>
+								<Link href="/studio">
+									<VideoIcon className="size-5" />
+									<span className="text-sm">Content</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+						<SidebarMenuItem>
+							<SidebarMenuButton tooltip={"Exit Studio"} asChild>
+								<Link href="/">
+									<LogOutIcon className="size-5" />
+									<span className="text-sm">Exit Studio</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					</SidebarMenu>
+				</SidebarGroup>
 			</SidebarContent>
 		</Sidebar>
 	);
