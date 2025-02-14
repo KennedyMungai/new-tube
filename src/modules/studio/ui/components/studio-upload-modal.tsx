211aca09495b2999745aa3b8e8fdd00cf2c0de5b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 11:31:02 +0300

    Assigned data to the open and onOpenChange props

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { ResponsiveModal } from "@/components/responsive-modal";
 import { Button } from "@/components/ui/button";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
@@ -18,16 +19,24 @@ export const StudioUploadModal = () => {
 	});
 
 	return (
-		<Button
-			variant={"secondary"}
-			onClick={() => create.mutate()}
-			disabled={create.isPending}>
-			{create.isPending ? (
-				<Loader2Icon className="size-5 animate-spin text-muted-foreground" />
-			) : (
-				<PlusIcon className="mr-2" />
-			)}
-			<span>Create</span>
-		</Button>
+		<>
+			<Button
+				variant={"secondary"}
+				onClick={() => create.mutate()}
+				disabled={create.isPending}>
+				{create.isPending ? (
+					<Loader2Icon className="size-5 animate-spin text-muted-foreground" />
+				) : (
+					<PlusIcon className="mr-2" />
+				)}
+				<span>Create</span>
+			</Button>
+			<ResponsiveModal
+				title="Upload a video"
+				open={!!create.data}
+				onOpenChange={() => create.reset()}>
+				<p>This will be an uploader</p>
+			</ResponsiveModal>
+		</>
 	);
 };
