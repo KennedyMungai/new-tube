1c784c175bdbaea7ff8365720ae0f9e3776dc66f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 09:40:26 +0300

    Made some updates

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -2,10 +2,8 @@ import { StudioView } from "@/modules/studio/ui/views/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 const StudioPage = async () => {
-	void trpc.categories.getMany
-		.prefetchInfinite
-		// { limit: DEFAULT_LIMIT }
-		();
+	// TODO: Remove if it causes a build error
+	void (trpc.categories.getMany.prefetchInfinite(/*{ limit: DEFAULT_LIMIT }*/));
 
 	return (
 		<HydrateClient>
