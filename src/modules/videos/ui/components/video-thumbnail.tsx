3f264d138403d677c633e97927ef523c43afd55b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 15:50:42 +0300

    Implemented the functionality to play animated preview of the video on hover

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -2,18 +2,26 @@ import Image from "next/image";
 
 type Props = {
 	imageUrl?: string | null;
+	previewUrl?: string | null;
+	title: string;
 };
 
-export const VideoThumbnail = ({ imageUrl }: Props) => {
+export const VideoThumbnail = ({ imageUrl, previewUrl, title }: Props) => {
 	return (
-		<div className="relative">
+		<div className="relative group">
 			{/* Thumbnail Wrapper */}
 			<div className="relative w-full overflow-hidden rounded-xl aspect-video">
 				<Image
 					src={imageUrl ?? "/placeholder.svg"}
 					alt="Thumbnail"
 					fill
-					className="size-full object-cover"
+					className="size-full object-cover group-hover:opacity-0 transition-opacity duration-300 opacity-100"
+				/>
+				<Image
+					src={previewUrl ?? "/placeholder.svg"}
+					alt="Thumbnail"
+					fill
+					className="size-full object-cover group-hover:opacity-100 transition-opacity duration-300 opacity-0"
 				/>
 			</div>
 
