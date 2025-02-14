bd8c1f5dce3c082cd734ee2a5578a6d3bbf51cd4
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 09:50:51 +0300

    Created, registered and implemented the videos create functionality

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { trpc } from "@/trpc/client";
 import { PlusIcon } from "lucide-react";
 
 export const StudioUploadModal = () => {
+	const create = trpc.videos.create.useMutation();
+
 	return (
-		<Button variant={"secondary"}>
+		<Button variant={"secondary"} onClick={() => create.mutate}>
 			<PlusIcon className="mr-2" />
 			<span>Create</span>
 		</Button>
--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,7 +1,6 @@
 import { db } from "@/db";
 import { videos } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { z } from "zod";
 
 export const videosRouter = createTRPCRouter({
 	create: protectedProcedure.mutation(async ({ ctx }) => {
