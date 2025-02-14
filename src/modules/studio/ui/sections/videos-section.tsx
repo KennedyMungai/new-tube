580c2d1887839d8b6381d5111e340cfe5ce8bd82
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 10:28:01 +0300

    Added some suspense and error handling for the UI and made sure the content renders on the client side

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -2,8 +2,26 @@
 
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { Suspense, useEffect, useState } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 export const VideosSection = () => {
+	const [isMounted, setIsMounted] = useState(false);
+
+	useEffect(() => setIsMounted(true), []);
+
+	if (!isMounted) return null;
+
+	return (
+		<Suspense fallback={<p>Loading...</p>}>
+			<ErrorBoundary fallback={<p>Error!</p>}>
+				<VideosSectionSuspense></VideosSectionSuspense>
+			</ErrorBoundary>
+		</Suspense>
+	);
+};
+
+const VideosSectionSuspense = () => {
 	const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery(
 		{ limit: DEFAULT_LIMIT },
 		{
