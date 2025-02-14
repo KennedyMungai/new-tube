d7d970d9e715f9dcb784ebf48f11f8e55db15397
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 09:52:01 +0300

    Fixed a dumb function

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -8,7 +8,7 @@ export const StudioUploadModal = () => {
 	const create = trpc.videos.create.useMutation();
 
 	return (
-		<Button variant={"secondary"} onClick={() => create.mutate}>
+		<Button variant={"secondary"} onClick={() => create.mutate()}>
 			<PlusIcon className="mr-2" />
 			<span>Create</span>
 		</Button>
