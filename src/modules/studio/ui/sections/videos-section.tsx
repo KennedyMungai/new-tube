77447b8e88e83e08b06f529b9f1d70b349bdaac3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 11:13:19 +0300

    Rendering a table of the videos on the database

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,8 +1,17 @@
 "use client";
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
+import {
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableHeader,
+	TableRow,
+} from "@/components/ui/table";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import Link from "next/link";
 import { Suspense, useEffect, useState } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 
@@ -23,7 +32,7 @@ export const VideosSection = () => {
 };
 
 const VideosSectionSuspense = () => {
-	const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+	const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
 		{ limit: DEFAULT_LIMIT },
 		{
 			getNextPageParam: (lastPage) => lastPage.nextCursor,
@@ -32,7 +41,42 @@ const VideosSectionSuspense = () => {
 
 	return (
 		<div>
-			{JSON.stringify(data)}
+			<div className="border-y">
+				<Table>
+					<TableHeader>
+						<TableRow>
+							<TableHead className="pl-6 w-[510px]">Video</TableHead>
+							<TableHead>Visibility</TableHead>
+							<TableHead>Status</TableHead>
+							<TableHead>Date</TableHead>
+							<TableHead className="text-right">Views</TableHead>
+							<TableHead className="text-right">Comments</TableHead>
+							<TableHead className="text-right pr-6">Likes</TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{videos.pages
+							.flatMap((page) => page.items)
+							.map((video) => (
+								// HACK: The legacy behavior prop solves the hydration error
+								<Link
+									href={`/studio/videos/${video.id}`}
+									key={video.id}
+									legacyBehavior>
+									<TableRow className="cursor-pointer">
+										<TableCell>{video.title}</TableCell>
+										<TableCell>Visibility</TableCell>
+										<TableCell>Status</TableCell>
+										<TableCell>Date</TableCell>
+										<TableCell>Views</TableCell>
+										<TableCell>Comments</TableCell>
+										<TableCell>Likes</TableCell>
+									</TableRow>
+								</Link>
+							))}
+					</TableBody>
+				</Table>
+			</div>
 			<InfiniteScroll
 				isManual
 				hasNextPage={query.hasNextPage}
