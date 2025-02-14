9e4a08cd7fbe51218e369da57ba934ab86077b80
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-14 09:41:13 +0300

    Updated the TODO to a HACK

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -2,7 +2,7 @@ import { StudioView } from "@/modules/studio/ui/views/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
 const StudioPage = async () => {
-	// TODO: Remove if it causes a build error
+	// HACK: Remove if it causes a build error
 	void (trpc.categories.getMany.prefetchInfinite(/*{ limit: DEFAULT_LIMIT }*/));
 
 	return (
