292e99b005418cbc9cc082fc2dac4fe5f0e406c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 09:38:45 +0300

    Added a function to convert snakeCase to Title case

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,10 @@ export function cn(...inputs: ClassValue[]) {
 export const formatDuration = (duration: number) => {
 	const seconds = Math.floor((duration % 60_000) / 1000);
 	const minutes = Math.floor(duration / 60_000);
-  const hours = Math.floor(duration / 3_600_000);
+	const hours = Math.floor(duration / 3_600_000);
 
 	return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
+
+export const snakeCaseToTitle = (str: string) =>
+	str.replace(/_/g, " ").replace(/\b\w/g, (char) => char.toUpperCase());
