b9f6af9711d5d93f9b25534a9899aeb0347654b3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 12:25:10 +0300

    Added prefetch for the video categories

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -11,6 +11,7 @@ const VideoPage = async ({ params }: Props) => {
 	const { videoId } = await params;
 
 	void trpc.studio.getOne.prefetch({ id: videoId });
+    void trpc.categories.getMany.prefetch();
 
 	return (
 		<HydrateClient>
