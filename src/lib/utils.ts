977399eeba15b9ccb3cadc63b2f8d4a3f651e3c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 09:16:29 +0300

    Added the hours option to the video duration

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,6 +8,7 @@ export function cn(...inputs: ClassValue[]) {
 export const formatDuration = (duration: number) => {
 	const seconds = Math.floor((duration % 60_000) / 1000);
 	const minutes = Math.floor(duration / 60_000);
+  const hours = Math.floor(duration / 3_600_000);
 
-	return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+	return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
