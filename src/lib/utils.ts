38929da025b31304edaafc3876809b65a4ab5066
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 09:12:12 +0300

    Replaced the modulus symbol with the division symbol

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export const formatDuration = (duration: number) => {
 	const seconds = Math.floor((duration % 60_000) / 1000);
-	const minutes = Math.floor(duration % 60_000);
+	const minutes = Math.floor(duration / 60_000);
 
 	return `${minutes.toString().padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
 };
