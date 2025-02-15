b1bce875f2642b50ce045ef5056488c22d1cecb9
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 11:14:38 +0300

    Added some right padding for the skeleton loader

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -79,7 +79,7 @@ const VideosSectionSkeleton = () => {
 								<TableCell className="text-right">
 									<Skeleton className="h-4 w-12 ml-auto" />
 								</TableCell>
-								<TableCell className="text-right">
+								<TableCell className="text-right pr-6">
 									<Skeleton className="h-4 w-12 ml-auto" />
 								</TableCell>
 							</TableRow>
