4b6171c6f689bc2785a21eb62c0fac513c25f7c5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-15 11:31:18 +0300

    Prefetching one video

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -1,9 +1,15 @@
+import { trpc } from "@/trpc/server";
+
+export const dynamic = "force-dynamic";
+
 type Props = {
 	params: Promise<{ videoId: string }>;
 };
 
 const VideoPage = async ({ params }: Props) => {
 	const { videoId } = await params;
+
+	void trpc.studio.getOne({ id: videoId });
 
 	return <div>{videoId}</div>;
 };
