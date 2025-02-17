f5ce989881b9a057e14497f73330bde8b479d1e5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 10:45:54 +0300

    Defined and implemented the logic to restore the riginal video thumbnail

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -101,6 +101,19 @@ const FormSectionSuspense = ({ videoId }: Props) => {
 		onError: (error) => toast.error(error.message),
 	});
 
+
+	const restoreThumbnail = trpc.videos.restoreThumbnail.useMutation({
+		onSuccess: () => {
+			utils.studio.getMany.invalidate();
+			utils.studio.getOne.invalidate({ id: videoId });
+
+			toast.success("Thumbnail restored successfully");
+
+			router.push("/studio");
+		},
+		onError: (error) => toast.error(error.message),
+	});
+
 	const form = useForm<z.infer<typeof videoUpdateSchema>>({
 		resolver: zodResolver(videoUpdateSchema),
 		defaultValues: video,
@@ -230,7 +243,10 @@ const FormSectionSuspense = ({ videoId }: Props) => {
 															<SparklesIcon className="size-5 mr-1 " />
 															AI generated
 														</DropdownMenuItem>
-														<DropdownMenuItem>
+														<DropdownMenuItem
+															onClick={() =>
+																restoreThumbnail.mutate({ id: videoId })
+															}>
 															<RotateCcwIcon className="size-5 mr-1 " />
 															Restore
 														</DropdownMenuItem>
