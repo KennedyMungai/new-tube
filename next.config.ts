5980b095bdc1599c4617de4fbe8b17217a9e22ae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 10:26:32 +0300

    Added the addresses of the images uploaded to uploadthing

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
 				protocol: "https",
 				hostname: "image.mux.com",
 			},
+			{
+				protocol: "https",
+				hostname: "utfs.io",
+			},
+			{
+				protocol: "https",
+				hostname: "seal.ingest.uploadthing.com",
+			},
 		],
 	},
 };
