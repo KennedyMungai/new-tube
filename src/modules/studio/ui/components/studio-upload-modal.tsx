b648c732252d2517d13b0ff15e55071ad96547b1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 08:42:49 +0300

    Routing the user to the video url on creation

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -5,9 +5,12 @@ import { Button } from "@/components/ui/button";
 import { StudioUploader } from "@/modules/studio/ui/components/studio-uploader";
 import { trpc } from "@/trpc/client";
 import { Loader2Icon, PlusIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { toast } from "sonner";
 
 export const StudioUploadModal = () => {
+	const router = useRouter();
+
 	const utils = trpc.useUtils();
 	const create = trpc.videos.create.useMutation({
 		onSuccess: () => {
@@ -18,6 +21,13 @@ export const StudioUploadModal = () => {
 			toast.error(error.message);
 		},
 	});
+
+	const onSuccess = () => {
+		if (!create.data?.video.id) return;
+
+		create.reset();
+		router.push(`/studio/videos/${create.data.video.id}`);
+	};
 
 	return (
 		<>
@@ -37,7 +47,7 @@ export const StudioUploadModal = () => {
 				open={!!create.data?.url}
 				onOpenChange={() => create.reset()}>
 				{create.data?.url ? (
-					<StudioUploader endpoint={create.data.url} onSuccess={() => {}} />
+					<StudioUploader endpoint={create.data.url} onSuccess={onSuccess} />
 				) : (
 					<Loader2Icon className="animate-spin text-muted-foreground size-5" />
 				)}
