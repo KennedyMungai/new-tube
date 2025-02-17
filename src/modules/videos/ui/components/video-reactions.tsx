1666de159509a733a4e808eba147ed0289e8dd3e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 14:17:01 +0300

    Created and rendered the video-reactions and video-menu components

---
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -0,0 +1,3 @@
+export const VideoReactions = () => {
+	return <div>Video Reactions</div>;
+};
--- src/modules/videos/ui/components/video-top-row.tsx
+++ src/modules/videos/ui/components/video-top-row.tsx
@@ -1,5 +1,7 @@
 import { VideoGetOneOutput } from "@/modules/videos/types";
+import { VideoMenu } from "@/modules/videos/ui/components/video-menu";
 import { VideoOwner } from "@/modules/videos/ui/components/video-owner";
+import { VideoReactions } from "@/modules/videos/ui/components/video-reactions";
 
 type Props = {
 	video: VideoGetOneOutput;
@@ -11,6 +13,10 @@ export const VideoTopRow = ({ video }: Props) => {
 			<h1 className="text-xl font-semibold">{video.title}</h1>
 			<div className="flex flex-col sm:flex-row sm:items-start sm:justify-between gap-4">
 				<VideoOwner user={video.user} videoId={video.id} />
+				<div className="flex overflow-x-auto sm:min-w-[calc(50% - 6px)] sm:justify-end sm:overflow-visible pb-2 -mb-2 sm:pb-0 sm:mb-0 gap-2">
+					<VideoReactions />
+					<VideoMenu />
+				</div>
 			</div>
 		</div>
 	);
