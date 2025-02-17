066fb7245ed07c6610dedd18686d0bf6ae917e1c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 14:07:08 +0300

    Created and rendered the user-info component

--- src/modules/videos/ui/components/VideoOwner.tsx
+++ src/modules/videos/ui/components/VideoOwner.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import { UserAvatar } from "@/components/user-avatar";
 import { usersSelectSchema } from "@/db/schema";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
 import { useAuth } from "@clerk/nextjs";
 import Link from "next/link";
 import { z } from "zod";
@@ -19,6 +20,7 @@ export const VideoOwner = ({ user, videoId }: Props) => {
 			<Link href={`/users/${user.id}`}>
 				<div className="flex items-center gap-3 min-w-0">
 					<UserAvatar imageUrl={user.imageUrl} name={user.name} size="lg" />
+					<UserInfo size={"lg"} name={user.name} />
 					<span className="text-sm text-muted-foreground line-clamp-1">
 						{/* TODO: Properly fill in the subscribers */}
 						{0} Subscribers
