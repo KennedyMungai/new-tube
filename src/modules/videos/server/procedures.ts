39c20e44dbc5f12407cbadb6f0624c564bb5d080
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 12:26:43 +0300

    Added a join with the users table

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db";
-import { videos, videoUpdateSchema } from "@/db/schema";
+import { users, videos, videoUpdateSchema } from "@/db/schema";
 import { mux } from "@/lib/mux";
 import {
 	baseProcedure,
@@ -7,7 +7,7 @@ import {
 	protectedProcedure,
 } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, eq } from "drizzle-orm";
+import { and, eq, getTableColumns } from "drizzle-orm";
 import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
@@ -16,8 +16,14 @@ export const videosRouter = createTRPCRouter({
 		.input(z.object({ id: z.string().uuid() }))
 		.query(async ({ input }) => {
 			const [existingVideo] = await db
-				.select()
+				.select({
+					...getTableColumns(videos),
+					user: {
+						...getTableColumns(users),
+					},
+				})
 				.from(videos)
+				.innerJoin(users, eq(videos.userId, users.id))
 				.where(eq(videos.id, input.id));
 
 			if (!existingVideo) throw new TRPCError({ code: "NOT_FOUND" });
