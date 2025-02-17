30f7bbe8f75bb05ffd43b6f78dfdaacf5d61a843
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-17 07:54:12 +0300

    Added the MuxPlayer t the VideoPlayer component

--- src/modules/videos/ui/components/video-player.tsx
+++ src/modules/videos/ui/components/video-player.tsx
@@ -1,8 +1,30 @@
+import MuxPlayer from "@mux/mux-player-react";
+
 type Props = {
-	playbackId: string | null;
-	thumbnailUrl: string | null;
+	playbackId?: string | null;
+	thumbnailUrl?: string | null;
+	autoPlay?: boolean;
+	onPlay?: () => void;
 };
 
-export const VideoPlayer = ({ playbackId, thumbnailUrl }: Props) => {
-	return <div></div>;
+export const VideoPlayer = ({
+	playbackId,
+	thumbnailUrl,
+	autoPlay,
+	onPlay,
+}: Props) => {
+	if (!playbackId) return null;
+
+	return (
+		<MuxPlayer
+			playbackId={playbackId}
+			poster={thumbnailUrl ?? "/placeholder.svg"}
+			playerInitTime={0}
+			autoPlay={autoPlay}
+			thumbnailTime={0}
+			accentColor="#FF2056"
+			onPlay={onPlay}
+			className="size-full object-contain"
+		/>
+	);
 };
