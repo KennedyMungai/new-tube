8c663fe4eab95955b64bd7f037813536779f48c0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-18 14:04:22 +0300

    Added some markup and styling for the comment-item

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,9 +1,37 @@
+import { UserAvatar } from "@/components/user-avatar";
 import { CommentGetManyOutput } from "@/modules/comments/types";
+import { formatDistanceToNow } from "date-fns";
+import Link from "next/link";
 
 type Props = {
-	comment: CommentGetManyOutput;
+	comment: CommentGetManyOutput[number];
 };
 
-export const CommentItem = ({}: Props) => {
-	return <div></div>;
+export const CommentItem = ({ comment }: Props) => {
+	return (
+		<div>
+			<div className="flex gap-4">
+				<Link href={`/users/${comment.userId}`}>
+					<UserAvatar
+						size="lg"
+						imageUrl={comment.user.imageUrl ?? "/user-placeholder.svg"}
+						name={comment.user.name}
+					/>
+				</Link>
+				<div className="flex-1 min-w-0">
+					<Link href={`/users/${comment.userId}`}>
+						<div className="flex items-center gap-2 mb-0.5">
+							<span className="font-medium text-sm pb-0.5">
+								{comment.user.name}
+							</span>
+							<span className="text-xs text-muted-foreground">
+								{formatDistanceToNow(comment.createdAt, { addSuffix: true })}
+							</span>
+						</div>
+					</Link>
+					<p className="text-sm">{comment.value}</p>
+				</div>
+			</div>
+		</div>
+	);
 };
