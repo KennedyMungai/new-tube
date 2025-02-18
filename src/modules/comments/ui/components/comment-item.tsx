f9774b9702e21420795cd0c7bdcee1941804fd9b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-18 13:47:21 +0300

    Created and rendered tge comment-item component

---
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -0,0 +1,9 @@
+import { CommentGetOneOutput } from "@/modules/comments/types";
+
+type Props = {
+	comment: CommentGetOneOutput;
+};
+
+export const CommentItem = ({}: Props) => {
+	return <div></div>;
+};
