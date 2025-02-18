913d41f402d4e0b88c2f82652c8b3df3ff9def78
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-18 12:36:26 +0300

    Added the videoId prop to the comments section

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,3 +1,13 @@
-export const CommentsSection = () => {
-	return <div>Comments Section</div>;
+"use client";
+
+import { trpc } from "@/trpc/client";
+
+type Props = {
+	videoId: string;
+};
+
+export const CommentsSection = ({ videoId }: Props) => {
+	const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId });
+
+	return <div>{JSON.stringify(comments)}</div>;
 };
