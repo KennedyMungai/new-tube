b6771edeb6d8f41d30b5b537c8bf0465b4a1fca8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-18 09:05:25 +0300

    Added the like and dislike count items to the existingVideo object

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,5 +1,11 @@
 import { db } from "@/db";
-import { users, videos, videoUpdateSchema, videoViews } from "@/db/schema";
+import {
+	users,
+	videoReactions,
+	videos,
+	videoUpdateSchema,
+	videoViews,
+} from "@/db/schema";
 import { mux } from "@/lib/mux";
 import {
 	baseProcedure,
@@ -22,6 +28,20 @@ export const videosRouter = createTRPCRouter({
 						...getTableColumns(users),
 					},
 					videoViews: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+					likeCount: db.$count(
+						videoReactions,
+						and(
+							eq(videoReactions.videoId, videos.id),
+							eq(videoReactions.type, "like"),
+						),
+					),
+					dislikeCount: db.$count(
+						videoReactions,
+						and(
+							eq(videoReactions.videoId, videos.id),
+							eq(videoReactions.type, "dislike"),
+						),
+					),
 				})
 				.from(videos)
 				.innerJoin(users, eq(videos.userId, users.id))
