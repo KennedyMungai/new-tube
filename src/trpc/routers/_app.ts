edf190ecb04cb58a051587c6393b85312c9a90c8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-18 09:53:22 +0300

    Added the videoReactions router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,9 +1,9 @@
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { studioRouter } from "@/modules/studio/server/procedures";
+import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
 import { createTRPCRouter } from "../init";
-import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 
 export const appRouter = createTRPCRouter({
 	categories: categoriesRouter,
