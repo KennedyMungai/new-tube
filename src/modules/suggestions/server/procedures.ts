1b97981ce7a44636a244203007289795d0154a56
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-19 15:19:23 +0300

    Only loading public videos as suggestions

--- src/modules/suggestions/server/procedures.ts
+++ src/modules/suggestions/server/procedures.ts
@@ -58,6 +58,7 @@ export const suggestionsRouter = createTRPCRouter({
 				.where(
 					and(
 						not(eq(videos.id, existingVideo.id)),
+						eq(videos.visibility, "public"),
 						existingVideo.categoryId
 							? eq(videos.categoryId, existingVideo.categoryId)
 							: undefined,
