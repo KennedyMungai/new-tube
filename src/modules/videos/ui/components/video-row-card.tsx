939da38699192ae67f050cfaa8d6409659d2b99b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-19 13:52:07 +0300

    Created and rendered the video-row-card

---
+++ src/modules/videos/ui/components/video-row-card.tsx
@@ -0,0 +1,116 @@
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { UserAvatar } from "@/components/user-avatar";
+import { cn } from "@/lib/utils";
+import { UserInfo } from "@/modules/users/ui/components/user-info";
+import { VideoGetManyOutput } from "@/modules/videos/types";
+import { VideoMenu } from "@/modules/videos/ui/components/video-menu";
+import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
+import { cva, type VariantProps } from "class-variance-authority";
+import Link from "next/link";
+
+const videoRowCardVariants = cva("group flex min-w-0", {
+	variants: {
+		size: {
+			default: "gap-4",
+			compact: "gap-2",
+		},
+	},
+	defaultVariants: {
+		size: "default",
+	},
+});
+
+const thumbnailVariants = cva("relative flex-none", {
+	variants: {
+		size: {
+			default: "w-[38%]",
+			compact: "w-[168px]",
+		},
+	},
+	defaultVariants: {
+		size: "default",
+	},
+});
+
+interface Props extends VariantProps<typeof videoRowCardVariants> {
+	data: VideoGetManyOutput["items"][number];
+	onRemove?: () => void;
+}
+
+export const VideoCardSkeleton = () => {
+	return <div>Skeleton</div>;
+};
+
+export const VideoRowCard = ({ size, data, onRemove }: Props) => {
+	return (
+		<div className={videoRowCardVariants({ size })}>
+			<Link href={`/videos/${data.id}`} className={thumbnailVariants({ size })}>
+				<VideoThumbnail
+					imageUrl={data.thumbnailUrl}
+					previewUrl={data.previewUrl}
+					title={data.title}
+					duration={data.duration}
+				/>
+			</Link>
+
+			{/* Info */}
+			<div className="flex-1 min-w-0">
+				<div className="flex justify-between gap-x-2">
+					<Link href={`/videos/${data.id}`} className="flex-1 min-w-0">
+						<h3
+							className={cn(
+								"font-medium line-clamp-2",
+								size === "compact" ? "text-sm" : "text-base",
+							)}>
+							{data.title}
+						</h3>
+						{size === "default" && (
+							<p className="text-xs text-muted-foreground mt-1">
+								{data.viewCount} views • {data.likeCount} likes •{" "}
+								{data.dislikeCount} dislikes
+							</p>
+						)}
+						{size === "default" && (
+							<>
+								<div className="flex items-center gap-2 mt-3">
+									<UserAvatar
+										size="sm"
+										imageUrl={data.user.imageUrl}
+										name={data.user.name}
+									/>
+									<UserInfo size="sm" name={data.user.name} />
+								</div>
+								<Tooltip>
+									<TooltipTrigger asChild>
+										<p className="text-xs text-muted-foreground w-fit line-clamp-2">
+											{data.description ?? "No description"}
+										</p>
+									</TooltipTrigger>
+									<TooltipContent
+										side="bottom"
+										align="center"
+										className="bg-black/70">
+										<p>From the video description</p>
+									</TooltipContent>
+								</Tooltip>
+							</>
+						)}
+						{size === "compact" && <UserInfo size="sm" name={data.user.name} />}
+						{size === "compact" && (
+							<p className="text-xs text-muted-foreground mt-1">
+								{data.viewCount} views • {data.likeCount} likes
+							</p>
+						)}
+					</Link>
+					<div className="flex-none">
+						<VideoMenu videoId={data.id} onRemove={onRemove} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { DEFAULT_LIMIT } from "@/constants";
+import { VideoRowCard } from "@/modules/videos/ui/components/video-row-card";
 import { trpc } from "@/trpc/client";
 
 type Props = {
@@ -18,5 +19,13 @@ export const SuggestionsSection = ({ videoId }: Props) => {
 		},
 	);
 
-	return <div>{JSON.stringify(suggestions)}</div>;
+	return (
+		<div className="flex flex-col gap-y-4 h-screen overflow-auto">
+			{suggestions.pages.flatMap((page) =>
+				page.items.map((video) => (
+					<VideoRowCard key={video.id} data={video} size="compact" />
+				)),
+			)}
+		</div>
+	);
 };
