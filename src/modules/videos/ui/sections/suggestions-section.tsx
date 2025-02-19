b1ad60559c55a2f14bd4021eb9f081e0724b48a0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-19 12:52:22 +0300

    Loading the video suggestions data

--- src/modules/videos/ui/sections/suggestions-section.tsx
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -1,3 +1,22 @@
-export const SuggestionsSection = () => {
-	return <div>Suggestions</div>;
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+
+type Props = {
+	videoId: string;
+};
+
+export const SuggestionsSection = ({ videoId }: Props) => {
+	const [suggestions] = trpc.suggestions.getMany.useSuspenseInfiniteQuery(
+		{
+			videoId,
+			limit: DEFAULT_LIMIT,
+		},
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+		},
+	);
+
+	return <div>{JSON.stringify(suggestions)}</div>;
 };
