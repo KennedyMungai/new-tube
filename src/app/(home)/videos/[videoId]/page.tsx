5c0db85d87107a0b572743911244c387616ecbed
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-19 12:35:10 +0300

    Added prefetching for the video suggestions

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -14,6 +14,10 @@ const VideoPage = async ({ params }: Props) => {
 		videoId,
 		limit: DEFAULT_LIMIT,
 	});
+	void trpc.suggestions.getMany.prefetchInfinite({
+		videoId,
+		limit: DEFAULT_LIMIT,
+	});
 
 	return (
 		<HydrateClient>
