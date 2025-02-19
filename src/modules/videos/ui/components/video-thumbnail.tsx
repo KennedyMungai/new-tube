80dc0ed837b64f41af8281b66514dae592b1c67f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-19 15:29:07 +0300

    Defined a video thumbnail skeleton component

--- src/modules/videos/ui/components/video-thumbnail.tsx
+++ src/modules/videos/ui/components/video-thumbnail.tsx
@@ -1,12 +1,21 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { formatDuration } from "@/lib/utils";
-import Image from "next/image";
 import { THUMBNAIL_FALLBACK } from "@/modules/videos/constants";
+import Image from "next/image";
 
 type Props = {
 	imageUrl?: string | null;
 	previewUrl?: string | null;
 	title: string;
 	duration: number;
+};
+
+export const VideoThumbnailSkeleton = () => {
+	return (
+		<div className="relative w-full overflow-hidden rounded-xl aspect-video">
+			<Skeleton className="size-full" />
+		</div>
+	);
 };
 
 export const VideoThumbnail = ({
@@ -37,7 +46,6 @@ export const VideoThumbnail = ({
 			<div className="absolute bottom-2 right-2 px-1 py-0.5 rounded bg-black/80 text-white text-xs font-medium">
 				{formatDuration(duration)}
 			</div>
-			<div></div>
 		</div>
 	);
 };
