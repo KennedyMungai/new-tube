8b84faa0b2cbb4336fcb4cc95c535b49e8ac8852
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-20 08:16:59 +0300

    Copied over a preexisting CategoriesSection

--- src/modules/search/ui/sections/categories-section.tsx
+++ src/modules/search/ui/sections/categories-section.tsx
@@ -1,7 +1,47 @@
+"use client";
+
+import { FilterCarousel } from "@/components/filter-carousel";
+import { trpc } from "@/trpc/client";
+import { useRouter } from "next/navigation";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 type Props = {
 	categoryId?: string;
 };
 
 export const CategoriesSection = ({ categoryId }: Props) => {
-	return <div>{categoryId}</div>;
+	return (
+		<Suspense fallback={<CategoriesSkeleton />}>
+			<ErrorBoundary fallback={<p>Error...</p>}>
+				<CategoriesSectionSuspense categoryId={categoryId} />
+			</ErrorBoundary>
+		</Suspense>
+	);
+};
+
+const CategoriesSkeleton = () => (
+	<FilterCarousel isLoading onSelect={() => {}} data={[]} />
+);
+
+const CategoriesSectionSuspense = ({ categoryId }: Props) => {
+	const [categories] = trpc.categories.getMany.useSuspenseQuery();
+
+	const router = useRouter();
+
+	const data = categories.map(({ name, id }) => ({
+		value: id,
+		label: name,
+	}));
+
+	const onSelect = (value: string | null) => {
+		const url = new URL(window.location.href);
+
+		if (value) url.searchParams.set("categoryId", value);
+		else url.searchParams.delete("categoryId");
+
+		router.push(url.toString());
+	};
+
+	return <FilterCarousel value={categoryId} data={data} onSelect={onSelect} />;
 };
