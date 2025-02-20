900f9e35e74eaca271bf28a4b2b578d9511eae51
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-20 11:33:04 +0300

    Rendering the home videos JSON info

--- src/modules/videos/ui/sections/home-videos-section.tsx
+++ src/modules/videos/ui/sections/home-videos-section.tsx
@@ -1,7 +1,38 @@
+"use client";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { trpc } from "@/trpc/client";
+import { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+
 type Props = {
 	categoryId?: string;
 };
 
 export const HomeVideosSection = ({ categoryId }: Props) => {
-	return <div>{categoryId}</div>;
+	return (
+		<Suspense fallback={<HomeVideosSectionSkeleton />}>
+			<ErrorBoundary fallback={<p>Error</p>}>
+				<HomeVideosSectionSuspense categoryId={categoryId} />
+			</ErrorBoundary>
+		</Suspense>
+	);
+};
+
+const HomeVideosSectionSkeleton = () => {
+	return <div>Loading...</div>;
+};
+
+const HomeVideosSectionSuspense = ({ categoryId }: Props) => {
+	const [videos, query] = trpc.videos.getMany.useSuspenseInfiniteQuery(
+		{
+			categoryId,
+			limit: DEFAULT_LIMIT,
+		},
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+		},
+	);
+
+	return <div>{JSON.stringify(videos)}</div>;
 };
