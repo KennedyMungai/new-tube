62c56a8c3565a9aa32edac065638ef2c693d9861
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-20 14:58:59 +0300

    Using css instead of the useMobile hook for responsiveness

--- src/modules/search/ui/sections/results-section.tsx
+++ src/modules/search/ui/sections/results-section.tsx
@@ -2,7 +2,6 @@
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
-import { useIsMobile } from "@/hooks/use-mobile";
 import {
 	VideoGridCard,
 	VideoGridCardSkeleton,
@@ -51,8 +50,6 @@ const ResultsSectionSkeleton = () => {
 };
 
 const ResultsSectionSuspense = ({ categoryId, query }: Props) => {
-	const isMobile = useIsMobile();
-
 	const [results, resultQuery] = trpc.search.getMany.useSuspenseInfiniteQuery(
 		{
 			query,
@@ -65,29 +62,26 @@ const ResultsSectionSuspense = ({ categoryId, query }: Props) => {
 	);
 
 	return (
-		<>
-			{isMobile ? (
-				<div className="flex flex-col gap-4 gap-y-10">
-					{results.pages
-						.flatMap((page) => page.items)
-						.map((video) => (
-							<VideoGridCard key={video.id} data={video} />
-						))}
-				</div>
-			) : (
-				<div className="flex flex-col gap-4">
-					{results.pages
-						.flatMap((page) => page.items)
-						.map((video) => (
-							<VideoRowCard key={video.id} data={video} />
-						))}
-				</div>
-			)}
+		<div>
+			<div className="flex flex-col gap-4 gap-y-10 md:hidden">
+				{results.pages
+					.flatMap((page) => page.items)
+					.map((video) => (
+						<VideoGridCard key={video.id} data={video} />
+					))}
+			</div>
+			<div className="md:flex hidden flex-col gap-4">
+				{results.pages
+					.flatMap((page) => page.items)
+					.map((video) => (
+						<VideoRowCard key={video.id} data={video} />
+					))}
+			</div>
 			<InfiniteScroll
 				hasNextPage={resultQuery.hasNextPage}
 				isFetchingNextPage={resultQuery.isFetchingNextPage}
 				fetchNextPage={resultQuery.fetchNextPage}
 			/>
-		</>
+		</div>
 	);
 };
