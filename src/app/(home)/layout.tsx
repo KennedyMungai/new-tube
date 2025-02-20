55627e1acf9ec0ffe392f065a12f41c6276e5988
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-20 10:29:58 +0300

    Made the layout files dynamic

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,8 @@
 import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
 import { ReactNode } from "react";
 
+export const dynamic = "force-dynamic";
+
 type Props = {
 	children: ReactNode;
 };
--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,6 +1,8 @@
 import { StudioLayout } from "@/modules/studio/ui/layouts/studio-layout";
 import { ReactNode } from "react";
 
+export const dynamic = "force-dynamic";
+
 type Props = {
 	children: ReactNode;
 };
