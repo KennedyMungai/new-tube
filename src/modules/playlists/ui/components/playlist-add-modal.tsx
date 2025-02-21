76a9c18cb9fcb0d76f38ae33a5eee22ea3bf6a03
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-21 12:40:44 +0300

    Updated the data fetching query

--- src/modules/playlists/ui/components/playlist-add-modal.tsx
+++ src/modules/playlists/ui/components/playlist-add-modal.tsx
@@ -1,97 +1,33 @@
+"use client";
+
 import { ResponsiveModal } from "@/components/responsive-modal";
-import { Button } from "@/components/ui/button";
-import {
-	Form,
-	FormControl,
-	FormField,
-	FormItem,
-	FormLabel,
-	FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { toast } from "sonner";
-import { z } from "zod";
 
 type Props = {
 	open: boolean;
+	videoId: string;
 	onOpenChange: (open: boolean) => void;
 };
 
-const formSchema = z.object({
-	name: z.string().min(1),
-});
-
-export const PlaylistAddModal = ({ onOpenChange, open }: Props) => {
-	const utils = trpc.useUtils();
-
-	const form = useForm<z.infer<typeof formSchema>>({
-		resolver: zodResolver(formSchema),
-		defaultValues: {
-			name: "",
+export const PlaylistAddModal = ({ onOpenChange, open, videoId }: Props) => {
+	const { data } = trpc.playlists.getManyForVideo.useInfiniteQuery(
+		{
+			limit: DEFAULT_LIMIT,
+			videoId,
 		},
-	});
-
-	const create = trpc.playlists.create.useMutation({
-		onSuccess: () => {
-			utils.playlists.getMany.invalidate();
-			toast.success("Playlist created", {
-				description: "Check your playlists",
-			});
-			form.reset();
-			onOpenChange(false);
+		{
+			getNextPageParam: (lastPage) => lastPage.nextCursor,
+			enabled: !!videoId && open,
 		},
-		onError: () => toast.error("Something went wrong"),
-	});
-
-	const onSubmit = async ({ name }: z.infer<typeof formSchema>) => {
-		await create.mutateAsync({
-			name,
-		});
-	};
+	);
 
 	return (
 		<ResponsiveModal
-			title="Create a playlist"
+			title="Add to playlist"
 			open={open}
 			onOpenChange={onOpenChange}>
-			<Form {...form}>
-				<form
-					onSubmit={form.handleSubmit(onSubmit)}
-					className="flex flex-col gap-4">
-					<FormField
-						control={form.control}
-						name="name"
-						render={({ field }) => (
-							<FormItem>
-								<FormLabel>Playlist Name</FormLabel>
-								<FormControl>
-									<Input
-										{...field}
-										className="resize-none"
-										placeholder="Playlist name"
-									/>
-								</FormControl>
-								<FormMessage />
-							</FormItem>
-						)}
-					/>
-					<div className="flex justify-end">
-						<Button
-							disabled={
-								form.formState.isSubmitting ||
-								create.isPending ||
-								!form.formState.isValid ||
-								!form.formState.isDirty
-							}
-							type="submit">
-							Create
-						</Button>
-					</div>
-				</form>
-			</Form>
+			<div className="flex flex-col gap-2">{JSON.stringify(data)}</div>
 		</ResponsiveModal>
 	);
 };
