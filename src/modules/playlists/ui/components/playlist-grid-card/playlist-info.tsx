a4d600bde72f5643eb3d60730665aed0aa653aae
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-21 10:30:45 +0300

    Defined the PlaylistInfoSkeleton component

--- src/modules/playlists/ui/components/playlist-grid-card/playlist-info.tsx
+++ src/modules/playlists/ui/components/playlist-grid-card/playlist-info.tsx
@@ -1,7 +1,20 @@
+import { Skeleton } from "@/components/ui/skeleton";
 import { PlaylistGetManyOutput } from "@/modules/playlists/types";
 
 type Props = {
 	data: PlaylistGetManyOutput["items"][number];
+};
+
+export const PlaylistInfoSkeleton = () => {
+	return (
+		<div className="flex gap-3">
+			<div className="min-w-0 flex-1 space-y-2">
+				<Skeleton className="h-5 w-[90%]" />
+				<Skeleton className="h-5 w-[70%]" />
+				<Skeleton className="h-5 w-[50%]" />
+			</div>
+		</div>
+	);
 };
 
 export const PlaylistInfo = ({ data }: Props) => {
