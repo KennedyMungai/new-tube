15bdabe62d528888ef8f1570bd9b7603d03af934
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-22 11:17:14 +0300

    Rendering the desktop user info page

--- src/modules/users/ui/components/user-page-info.tsx
+++ src/modules/users/ui/components/user-page-info.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { UserAvatar } from "@/components/user-avatar";
+import { cn } from "@/lib/utils";
 import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
 import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
 import { UsersGetOneOutput } from "@/modules/users/types";
@@ -36,7 +37,7 @@ export const UserPageInfo = ({ user }: Props) => {
 						className="size-[60px]"
 					/>
 					<div className="flex-1 min-w-0">
-						<h1 className="text-xl font-bold">{user.name}</h1>
+						<h1 className="text-xl font-bold capitalize">{user.name}</h1>
 						<div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
 							<span>{user.subscriberCount} subscribers</span>
 							<span>&bull;</span>
@@ -60,6 +61,46 @@ export const UserPageInfo = ({ user }: Props) => {
 						className="mt-3 w-full"
 					/>
 				)}
+			</div>
+
+			{/* Desktop Layout */}
+			<div className="md:flex hidden items-start gap-4">
+				<UserAvatar
+					imageUrl={user.imageUrl}
+					size="xl"
+					name={user.name}
+					onClick={() => {
+						if (user.clerkId === userId) {
+							clerk.openUserProfile();
+						}
+					}}
+					className={cn(
+						userId === user.clerkId
+							? "cursor-pointer hover:opacity-80 transition-opacity duration-300"
+							: "",
+					)}
+				/>
+				<div className="flex-1 min-w-0">
+					<h1 className="text-4xl font-bold capitalize">{user.name}</h1>
+					<div className="flex items-center gap-1 text-sm text-muted-foreground mt-3">
+						<span>{user.subscriberCount} subscribers</span>
+						<span>&bull;</span>
+						<span>{user.videoCount} videos</span>
+					</div>
+					{userId === user.clerkId ? (
+						<Button variant={"secondary"} asChild className="mt-3 rounded-full">
+							<Link href="/studio">Go to studio</Link>
+						</Button>
+					) : (
+						<SubscriptionButton
+							disabled={isPending || !isLoaded}
+							onClick={onClick}
+							isSubscribed={user.viewerSubscribed}
+							size={"default"}
+							className="mt-3"
+						/>
+					)}
+				</div>
 			</div>
 		</div>
 	);
