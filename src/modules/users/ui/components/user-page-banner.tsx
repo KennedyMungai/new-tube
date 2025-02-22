0cd299b519621dc6802411a5dc14eab28cbb8f78
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-22 10:04:12 +0300

    Added a TODO to add an upload banner modal

--- src/modules/users/ui/components/user-page-banner.tsx
+++ src/modules/users/ui/components/user-page-banner.tsx
@@ -14,6 +14,7 @@ export const UserPageBanner = ({ user }: Props) => {
 
 	return (
 		<div className="relative group">
+			{/* TODO: Add upload banner modal */}
 			<div
 				className={cn(
 					"w-full max-h-[200px] h-[15vh] md:h-[25vh] bg-gradient-to-r from-gray-100 to-gray-200 rounded-xl",
