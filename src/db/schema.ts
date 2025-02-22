08b37afc350a76f07eb9d0315ccd73468d33301a
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-22 08:59:04 +0300

    Added the bannerUrl and bannerKey fields to the users table

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -30,8 +30,8 @@ export const users = pgTable(
 		id: uuid("id").defaultRandom().primaryKey(),
 		clerkId: varchar("clerk_id", { length: 255 }).unique().notNull(),
 		name: varchar("name", { length: 255 }).notNull(),
-		// TODO: Add banner fields
-		// bannerUrl: varchar("banner_url", { length: 255 }).notNull(),
+		bannerUrl: varchar("banner_url", { length: 255 }),
+		bannerKey: varchar("banner_key", { length: 255 }),
 		imageUrl: varchar("image_url", { length: 512 }).notNull(),
 		createdAt: timestamp("created_at").defaultNow().notNull(),
 		updatedAt: timestamp("updated_at").defaultNow().notNull(),
