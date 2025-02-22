670b1a15369f6c0cdeb235433da74fa469064080
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-22 10:48:45 +0300

    Wrote the user mobile view

--- src/modules/users/ui/components/user-page-info.tsx
+++ src/modules/users/ui/components/user-page-info.tsx
@@ -1,9 +1,66 @@
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
+import { useSubscription } from "@/modules/subscriptions/hooks/use-subscription";
+import { SubscriptionButton } from "@/modules/subscriptions/ui/components/subscription-button";
 import { UsersGetOneOutput } from "@/modules/users/types";
+import { useAuth, useClerk } from "@clerk/nextjs";
+import Link from "next/link";
 
 type Props = {
 	user: UsersGetOneOutput;
 };
 
 export const UserPageInfo = ({ user }: Props) => {
-	return <div>{JSON.stringify(user)}</div>;
+	const clerk = useClerk();
+	const { userId, isLoaded } = useAuth();
+
+	const { isPending, onClick } = useSubscription({
+		userId: user.id,
+		isSubscribed: user.viewerSubscribed,
+	});
+
+	return (
+		<div className="py-6">
+			{/* Mobile Layout */}
+			<div className="flex flex-col md:hidden">
+				<div className="flex items-center gap-3">
+					<UserAvatar
+						imageUrl={user.imageUrl}
+						size="lg"
+						name={user.name}
+						onClick={() => {
+							if (user.clerkId === userId) {
+								clerk.openUserProfile();
+							}
+						}}
+						className="size-[60px]"
+					/>
+					<div className="flex-1 min-w-0">
+						<h1 className="text-xl font-bold">{user.name}</h1>
+						<div className="flex items-center gap-1 text-xs text-muted-foreground mt-1">
+							<span>{user.subscriberCount} subscribers</span>
+							<span>&bull;</span>
+							<span>{user.videoCount} videos</span>
+						</div>
+					</div>
+				</div>
+				{userId === user.clerkId ? (
+					<Button
+						variant={"secondary"}
+						asChild
+						className="w-full mt-3 rounded-full">
+						<Link href="/studio">Go to studio</Link>
+					</Button>
+				) : (
+					<SubscriptionButton
+						disabled={isPending || !isLoaded}
+						onClick={onClick}
+						isSubscribed={user.viewerSubscribed}
+						size={"default"}
+						className="mt-3 w-full"
+					/>
+				)}
+			</div>
+		</div>
+	);
 };
