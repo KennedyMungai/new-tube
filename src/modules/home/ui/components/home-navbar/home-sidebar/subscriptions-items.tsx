ca8bf1a81129c200cd1e02c37836a7486a73209c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-24 08:16:34 +0300

    Implemented a subscriptions skeleton loader and a sidebutton link to the subscriptions age

--- src/modules/home/ui/components/home-navbar/home-sidebar/subscriptions-items.tsx
+++ src/modules/home/ui/components/home-navbar/home-sidebar/subscriptions-items.tsx
@@ -8,15 +8,38 @@ import {
 	SidebarMenuButton,
 	SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
 import { UserAvatar } from "@/components/user-avatar";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
+import { ListIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 
+export const SubscriptionItemsLoadingSkeleton = () => {
+	return (
+		<>
+			{Array.from({ length: 4 }).map((_, index) => (
+				<SidebarMenuItem key={index}>
+					<SidebarMenuButton
+						disabled
+						tooltip="Loading..."
+						isActive={false}
+						asChild>
+						<div className="flex items-center gap-4">
+							<Skeleton className="size-6 rounded-full shrink-0" />
+							<Skeleton className="h-4 w-full" />
+						</div>
+					</SidebarMenuButton>
+				</SidebarMenuItem>
+			))}
+		</>
+	);
+};
+
 export const SubscriptionsItems = () => {
 	const pathname = usePathname();
-	const { data } = trpc.subscriptions.getMany.useInfiniteQuery(
+	const { data, isLoading } = trpc.subscriptions.getMany.useInfiniteQuery(
 		{
 			limit: DEFAULT_LIMIT,
 		},
@@ -30,28 +53,45 @@ export const SubscriptionsItems = () => {
 			<SidebarGroupLabel>Subscriptions</SidebarGroupLabel>
 			<SidebarGroupContent>
 				<SidebarMenu>
-					{data?.pages
-						.flatMap((page) => page.items)
-						.map((subscription) => (
-							<SidebarMenuItem
-								key={`${subscription.creatorId}-${subscription.viewerId}`}>
-								<SidebarMenuButton
-									tooltip={subscription.user.name}
-									isActive={pathname === `/users/${subscription.user.id}`}
-									asChild>
-									<Link
-										href={`/users/${subscription.user.id}`}
-										className="flex items-center gap-4">
-										<UserAvatar
-											imageUrl={subscription.user.imageUrl}
-											name={subscription.user.name}
-											size={"sm"}
-										/>
-										<span className="text-sm">{subscription.user.name}</span>
-									</Link>
-								</SidebarMenuButton>
-							</SidebarMenuItem>
-						))}
+					{isLoading ? (
+						<SubscriptionItemsLoadingSkeleton />
+					) : (
+						data?.pages
+							.flatMap((page) => page.items)
+							.map((subscription) => (
+								<SidebarMenuItem
+									key={`${subscription.creatorId}-${subscription.viewerId}`}>
+									<SidebarMenuButton
+										tooltip={subscription.user.name}
+										isActive={pathname === `/users/${subscription.user.id}`}
+										asChild>
+										<Link
+											href={`/users/${subscription.user.id}`}
+											className="flex items-center gap-4">
+											<UserAvatar
+												imageUrl={subscription.user.imageUrl}
+												name={subscription.user.name}
+												size={"sm"}
+											/>
+											<span className="text-sm">{subscription.user.name}</span>
+										</Link>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))
+					)}
+					{!isLoading && (
+						<SidebarMenuItem className="mt-2">
+							<SidebarMenuButton
+								asChild
+								isActive={pathname === "/subscriptions"}
+								tooltip={"All subscriptions"}>
+								<Link href="/subscriptions">
+									<ListIcon className="size-4" />
+									<span className="text-sm">All subscriptions</span>
+								</Link>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					)}
 				</SidebarMenu>
 			</SidebarGroupContent>
 		</SidebarGroup>
