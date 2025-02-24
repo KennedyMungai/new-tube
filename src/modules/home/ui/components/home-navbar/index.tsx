6f85c253144da06bc8f170c41a7080370945576b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-24 09:09:00 +0300

    Made the searchbar responsive

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -11,7 +11,7 @@ export const HomeNavbar = () => {
 				{/* Menu and logo */}
 				<div className="flex items-center flex-shrink-0">
 					<SidebarTrigger />
-					<Link href="/">
+					<Link href="/" className="hidden md:block">
 						<div className="p-4 flex items-center gap-1">
 							<Image src="/logo.svg" alt="logo" width={32} height={32} />
 							<p className="text-xl font-semibold tracking-tight">NewTube</p>
@@ -24,7 +24,7 @@ export const HomeNavbar = () => {
 					<SearchInput />
 				</div>
 			</div>
-			<div className="flex-shrink-0 items-center flex gap-4">
+			<div className="flex-shrink-0 items-center flex gap-4 ml-2">
 				<AuthButton />
 			</div>
 		</nav>
