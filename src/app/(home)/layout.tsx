4f0f415f921440b14257ee5db42faf661d1d5d20
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-24 09:00:17 +0300

    Dynamic rendering not needed for the HomeLayout file

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,6 @@
 import { HomeLayout } from "@/modules/home/ui/layouts/home-layout";
 import { ReactNode } from "react";
 
-// TODO: Check whether this is needed
-export const dynamic = "force-dynamic";
-
 type Props = {
 	children: ReactNode;
 };
