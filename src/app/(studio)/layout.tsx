bdbe73514dc3eb3400025791b4d7b5a3e35c0fe3
Kennedy Mungai <kennedymungaifmab@gmail.com>
2025-02-24 09:01:13 +0300

    Removed the force dynamic statement

--- src/app/(studio)/layout.tsx
+++ src/app/(studio)/layout.tsx
@@ -1,9 +1,6 @@
 import { StudioLayout } from "@/modules/studio/ui/layouts/studio-layout";
 import { ReactNode } from "react";
 
-// TODO: Check whether this is needed
-export const dynamic = "force-dynamic";
-
 type Props = {
 	children: ReactNode;
 };
